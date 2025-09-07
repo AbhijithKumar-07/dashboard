dd5cb1da8a9aaf6871af230dc482d3bb3be2f293
AbhijithKumar-07 <abhiyandrapu1106@gmail.com>
2025-09-07 14:43:03 +0530

    Final Changes

--- client/src/auth.js
+++ client/src/auth.js
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA64tFiEvx2_2MOleWrClBVT6PM4mVROus",
-  authDomain: "aerosystem-2d36c.firebaseapp.com",
-  projectId: "aerosystem-2d36c",
-  storageBucket: "aerosystem-2d36c.firebasestorage.app",
-  messagingSenderId: "1052847495761",
-  appId: "1:1052847495761:web:391e9f947d0435eae43670",
-  measurementId: "G-23JT3MPBL7"
+  apiKey: "AIzaSyANhgZF1wAAD24-JDZ780RDRs0i_0-sPz0",
+  authDomain: "customerapp-f50ff.firebaseapp.com",
+  projectId: "customerapp-f50ff",
+  storageBucket: "customerapp-f50ff.firebasestorage.app",
+  messagingSenderId: "681143401691",
+  appId: "1:681143401691:web:10c411cde54e9c912c60fe"
 };
 
 // Initialize Firebase
